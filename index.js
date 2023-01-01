6443e2a8a7d2e726f358aeaaaa901f0b98f56f8e
Darla Young <me3u2@hotmail.com>
2023-01-01 16:33:59 -0600

    task 2 + stretch complete, 15 tests passing

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function Person(name, age) {
 }
 
 Person.prototype.eat = function(someFood) {
-  if(this.stomach.length < 10) {
+  if (this.stomach.length < 10) {
     this.stomach.push(someFood)
   }
 }
@@ -51,10 +51,28 @@ Person.prototype.toString = function() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.model = model,
+  this.milesPerGallon = milesPerGallon,
+  this.tank = 0,
+  this.odometer = 0
 }
 
+Car.prototype.fill = function(gallons) {
+  this.tank = this.tank + gallons
+}
+
+Car.prototype.drive = function(distance) {
+  const maxDistance = this.tank * this.milesPerGallon
+  if (distance <= maxDistance) {
+    this.odometer = this.odometer + distance,
+    this.tank = this.tank - distance * this.milesPerGallon
+  } else {
+    this.odometer = this.odometer + maxDistance,
+    this.tank = 0;
+    return `I ran out of fuel at ${this.odometer} miles!`
+  }
+}
 
 /*
   TASK 3
